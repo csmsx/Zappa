639494fb523edd93e3f6fa6f56cb0c8412dd683f
Eric Platon <zaraki@gmx.com>
2017-11-14 17:21:14 +0900

    Sabotage removing TOML support, see if update works again.

--- zappa/cli.py
+++ zappa/cli.py
@@ -34,7 +34,6 @@ import string
 import sys
 import tempfile
 import time
-import toml
 import yaml
 import zipfile
 
@@ -2040,25 +2039,21 @@ class ZappaCLI(object):
 
     def get_json_or_yaml_settings(self, settings_name="zappa_settings"):
         """
-        Return zappa_settings path as JSON or YAML (or TOML), as appropriate.
+        Return zappa_settings path as JSON or YAML, as appropriate.
         """
         zs_json = settings_name + ".json"
         zs_yml = settings_name + ".yml"
         zs_yaml = settings_name + ".yaml"
-        zs_toml = settings_name + ".toml"
 
         # Must have at least one
         if not os.path.isfile(zs_json) \
             and not os.path.isfile(zs_yml) \
             and not os.path.isfile(zs_yaml) \
-            and not os.path.isfile(zs_toml):
             raise ClickException("Please configure a zappa_settings file or call `zappa init`.")
 
         # Prefer JSON
         if os.path.isfile(zs_json):
             settings_file = zs_json
-        elif os.path.isfile(zs_toml):
-            settings_file = zs_toml
         elif os.path.isfile(zs_yml):
             settings_file = zs_yml
         else:
@@ -2083,12 +2078,6 @@ class ZappaCLI(object):
                     self.zappa_settings = yaml.load(yaml_file)
                 except ValueError: # pragma: no cover
                     raise ValueError("Unable to load the Zappa settings YAML. It may be malformed.")
-        elif ext == '.toml':
-            with open(settings_file) as toml_file:
-                try:
-                    self.zappa_settings = toml.load(toml_file)
-                except ValueError: # pragma: no cover
-                    raise ValueError("Unable to load the Zappa settings TOML. It may be malformed.")
         else:
             with open(settings_file) as json_file:
                 try:
